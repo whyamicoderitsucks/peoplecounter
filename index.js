7da251c96133c8c1690f183fdbd2f82f764f682a
whyamicoderitsucks <shinetina169@gmail.com>
2024-09-01 21:12:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let entry = count + " - "
+    saveEl.textContent += entry
+    countEl.textContent =0
+    count =0
+    
+}
+
